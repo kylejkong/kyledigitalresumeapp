1ad0ed02323a2dbbee4eded4f5204f661b58b82e
kylejkong <85398390+kylejkong@users.noreply.github.com>
2023-03-28 13:17:12 +0800

    runtime

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
